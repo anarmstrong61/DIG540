aec32e4a9b6bdbce4019368fa6ef82ff33802593
anarmstrong61 <ashley.armstrong@maine.edu>
2022-09-19 09:56:00 -0500

    Add vocabulary.php to repository

---
+++ vocabulary.php
@@ -0,0 +1,17 @@
+<?php
+$content = file_get_contents("https://raw.githubusercontent.com/anarmstrong61/DIG540/main/02/vocabulary.json");
+$content = json_decode($content);
+echo "<table>";
+echo "<tr><th>Term</th><th>Wikipedia definition</th></tr>";
+foreach ($content as $row) {
+    echo "<tr>";
+    echo "<td>";
+    echo $row->name;
+    echo "</td>";
+    echo "<td>";
+    echo $row->definition;
+    echo "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
